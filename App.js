f7b6ef7769592dc0e75c0a854a2bccd08e2f1354
theafolayan <theafolayan@gmail.com>
2023-06-05 12:05:50 +0100

    Installed stockblok javascript SDK

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StoryblokClient from "storyblok-js-client";
+
+const Storyblok = new StoryblokClient({
+  accessToken: "bJQrwmwzfdfhYHlZQ3YuYwtt",
+});
+
+Storyblok.getAll("cdn/stories", {
+  starts_with: "articles/",
+})
+  .then((response) => {
+    console.log("ARTICLES", response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 export default function App() {
   return (
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
